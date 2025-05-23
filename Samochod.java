3b9ba522bcc524bad068b6f4e12433c674852eec
1zkawalizka <91870@student.pb.edu.pl>
2025-05-23 14:44:55 +0200

    dziedziczenie Samochod.java z absatrkcyjnej klasy pojazd
    Samochod:
    private int  ilość miejsc
    private boolean czymaLPG
    
    metody
    czy może wjechać - na podstawie lpg
    czy naliczamy dodatkowa oplate o 5zl za godzine, jeżeli liczba miejsc jest powyżej 8 osob

---
+++ Samochod.java
@@ -0,0 +1,27 @@
+public class Samochod extends Pojazd
+{
+    private boolean czyMaLPG;
+    private int iloscMiejsc;
+
+    public Samochod(String numerRejestracyjny, String marka, String model, LocalDateTime czasWjazdu,boolean czyMaLPG, int iloscMiejsc )
+    {
+        super(numerRejestracyjny,marka,model,czasWjazdu);
+        this.czyMaLPG=czyMaLPG;
+        this.iloscMiejsc=iloscMiejsc;
+    }
+   public boolean czyDozwolonyWjazd()
+   {
+    return !czyMaLPG;
+   }
+
+    @Override
+   public double obliczOplate()
+   {
+    double ileZaGodzine=super.obliczOplate();
+    if(iloscMiejsc>8)
+    {
+       ileZaGodzine+=5;
+    }
+      return ileZaGodzine;
+   }
+}
