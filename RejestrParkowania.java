06e5a351338b37bf2321f6e9281ae7f5208a9242
sadowski-damian <neonrideeer@gmail.com>
2025-05-30 21:47:33 +0200

    Stworzono klase RejestrParkowania.java i stworzono metody oraz konstruktor

--- RejestrParkowania.java
+++ RejestrParkowania.java
@@ -1,5 +1,25 @@
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+
 public class RejestrParkowania {
+    private LocalDateTime czasWjazdu; //
+    private LocalDateTime czasWyjazdu;
+    private Pojazd pojazd;
+    private double oplata;
+
+    public RejestrParkowania(Pojazd pojazd) {
+        this.czasWjazdu = pojazd.getCzasWjazdu();
+        this.czasWyjazdu = pojazd.getCzasWyjazdu();
+        this.pojazd = pojazd;
+        this.oplata = pojazd.getOplata();
+    }
+
+    public Duration pobierzCzasPostoju(){
+        Duration czasPostoju = Duration.between(czasWjazdu, czasWyjazdu);
+        return czasPostoju;
+    }
+
     public static void main(String[] args) {
-        System.out.println("test");
     }
 }
